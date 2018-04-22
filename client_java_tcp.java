cc9d6a4961a639f921078c44aba449e5476310a7
Blake Johnson <brjohnson61@gmail.com>
2018-04-22 06:32:50 -0700

    reverted error checking to test

--- client_java_tcp.java
+++ client_java_tcp.java
@@ -55,25 +55,25 @@ class client_java_tcp{
         System.out.print("Enter server name or IP address: ");
         scanner = new Scanner(System.in);
         IPAddress = scanner.nextLine();
-        String [] parsed = IPAddress.split(".");
+        //String [] parsed = IPAddress.split(".");
         //Check to see if there aren't 4 sections of IP Address.
-        if(parsed.length != 4){
-            System.out.println(parsed.length);
-            System.out.println(COULD_NOT_CONNECT);
-            System.exit(0);
-        }
-        //Checks to make sure ip is in valid range for each section.
-        else{
-            for(int i=0; i<parsed.length; i++){
-                Integer temp = Integer.valueOf(parsed[i]);
-                System.out.print("Temp = ");
-                System.out.println(temp);
-                if(temp > 255 || temp < 0){
-                    System.out.println(COULD_NOT_CONNECT);
-                    System.exit(0);
-                }
-            }
-        }
+        // if(parsed.length != 4){
+        //     System.out.println(parsed.length);
+        //     System.out.println(COULD_NOT_CONNECT);
+        //     System.exit(0);
+        // }
+        // //Checks to make sure ip is in valid range for each section.
+        // else{
+        //     for(int i=0; i<parsed.length; i++){
+        //         Integer temp = Integer.valueOf(parsed[i]);
+        //         System.out.print("Temp = ");
+        //         System.out.println(temp);
+        //         if(temp > 255 || temp < 0){
+        //             System.out.println(COULD_NOT_CONNECT);
+        //             System.exit(0);
+        //         }
+        //     }
+        // }
 
         System.out.print("Enter server port number:");
         port = Integer.valueOf(scanner.nextLine());
@@ -85,21 +85,21 @@ class client_java_tcp{
         System.out.print("Enter Command: ");
         command = scanner.nextLine();
         client_java_tcp client = new client_java_tcp();
-        String [] cmdparse = command.split(" > ");
+        // String [] cmdparse = command.split(" > ");
 
-        if(cmdparse.length > 1){
-            System.out.print("Command is: ");
-            System.out.println(cmdparse[0]);
+        // if(cmdparse.length > 1){
+        //     System.out.print("Command is: ");
+        //     System.out.println(cmdparse[0]);
 
-            System.out.print("Filename is: ");
-            System.out.println(cmdparse[1]);
-            client.connect(IPAddress, port, cmdparse[0], cmdparse[1]);
-        }
-        else{
+        //     System.out.print("Filename is: ");
+        //     System.out.println(cmdparse[1]);
+        //     client.connect(IPAddress, port, cmdparse[0], cmdparse[1]);
+        // }
+        // else{
             System.out.print("Command is: ");
             System.out.println(command);
             client.connect(IPAddress, port, command, fileName);
-        }
+        //}
 
         
         
